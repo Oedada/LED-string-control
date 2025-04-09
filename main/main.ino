f82b04a822d94f93e15b58796c47f8fdaae6b281
Oedada <163449434+Oedada@users.noreply.github.com>
2025-04-09 16:01:05 +0300

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,60 +1,60 @@
-#include <Adafruit_NeoPixel.h>
-#define countPixel 24
-#define pinString 2
-Adafruit_NeoPixel led(countPixel, pinString, NEO_GRB + NEO_KHZ800);
+#include <FastLED.h>
+
+#define LED_PIN  2
+#define NUM_LEDS 30
+#define COLOR_ORDER GRB
+#define CHIPSET     WS2811
+
+#define BRIGHTNESS 120
+CRGB leds[NUM_LEDS];
+
 void setup() {
-  Serial.begin(9600);
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+  Serial.begin(115200);
   Serial.println("--------------New output--------------");
-  led.begin();
-  led.setBrightness(100);
-  led.show();
+  FastLED.show();
 }
 
 void breath(float t, int r, int g, int b,int repeat){
-  int dl = ((float(t-0.561))/ 510.0)*1000000;
+  uint32_t startt = millis();
+  int y = 255;
+  long dl = t/(y*2);
   for(int j = 0; j < repeat; j++){
-    for(byte i = 0; i < 255; i++){
-      led.fill(led.Color(r, g, b), 0, 24);
-      led.setBrightness(i);
-      led.show();
-      delayMicroseconds(dl);
+    for(int i = 0; i < y; i += 5){
+      FastLED.delay(dl);
+      FastLED.showColor(CRGB(r, g, b), i);
+      FastLED.show();
     }
-    for(byte i = 255; i > 0; i--){
-      led.fill(led.Color(r, g, b), 0, 24);
-      led.setBrightness(i);
-      led.show();
-      delayMicroseconds(dl);
+    for(int i = y; i > 0; i -= 5){
+      FastLED.delay(dl);
+      FastLED.showColor(CRGB(r, g, b), i);
+      FastLED.show();
     }
   }
-  led.clear();
+  Serial.println(millis() - startt);
 }
 
 void staticRainbow(bool dir){
-  if(dir){
-    for(byte i = 0; i < countPixel; i++){
-      long hue = long((float(i)/24)*65535);
-      led.setPixelColor(i, led.ColorHSV(hue, 255, 255));
+    if(dir){
+      for(byte i = 0; i < NUM_LEDS; i++){
+        byte hue = float(i)/NUM_LEDS*191;
+        leds[i] = CHSV(hue, 255, BRIGHTNESS);
+      }
     }
-    led.show();
-  }
-  else{
-    for(byte i = 0; i < countPixel; i++){
-      long hue = long((float(countPixel-1-i)/24)*65535);
-      Serial.print(i); Serial.print(" ");
-      Serial.println(hue);
-      led.setPixelColor(i, led.ColorHSV(hue, 255, 255));
+    else{
+      for(byte i = 0; i < NUM_LEDS; i++){
+        byte hue = float(NUM_LEDS-i)/NUM_LEDS*191;
+        leds[i] = CHSV(hue, 255, BRIGHTNESS);
+      }
     }
-    led.show();
-  }
+    FastLED.show();
 }
 
-void showAll(){
-  breath(1, 50, 130, 10, 5);
-  staticRainbow(true);
-}
+//void showAll(){
+//  breath(1, 50, 130, 10, 5);
+//  staticRainbow(true);
+//}
 
 void loop() {
-//  showAll();
-  staticRainbow(false);
-  delay(5000);
+  staticRainbow(0);
 }
